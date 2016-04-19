16b54a9c566d764b51de125f30ec2658a3268103
Chinmay Khasnis <khas.chinmay@gmail.com>
2016-04-18 23:36:35 -0400

    Adding some options to Header...

--- rent_ui/src/app/components/Header.js
+++ rent_ui/src/app/components/Header.js
@@ -8,15 +8,20 @@ import {Button, ButtonToolbar} from 'react-bootstrap';
 
 var Header = React.createClass({
   render: function() {
+	  var renterStyle = {marginTop: '9px'};
     return(      
     		<nav className="navbar navbar-default">
-    		  <div className="container-fluid">
-    		    <div className="navbar-header">
-    		      <a className="navbar-brand" href="#">    		      
-    		        <div className="container">
-    		        	<span className="glyphicon glyphicon-home" />
-    		        </div>
-    		      </a>
+    		  <div className="container-fluid">    		    
+    		    <div className="navbar-header">    		      
+    		      <a className="navbar-brand" href="#">Brand</a>
+    		    </div>    		    
+    		    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">    		         		      
+    		      <ul className="nav navbar-nav navbar-right">
+    		      	<li><Button style={renterStyle} className="btn-warning outline" bsSize="small">Become a Renter</Button></li>    		      	
+    		        <li><a href="#">Help</a></li>
+    		        <li><a href="#">Sign Up</a></li>
+    		        <li><a href="#">Log In</a></li>
+    		      </ul>
     		    </div>
     		  </div>
     		</nav>
